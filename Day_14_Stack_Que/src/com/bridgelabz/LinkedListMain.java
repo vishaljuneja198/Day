9ac6cf974c93d4bd2619884a47ab39f58120a369
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-06-26 13:44:48 +0530

    master welcome method

---
+++ Day_14_Stack_Que/src/com/bridgelabz/LinkedListMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Data Structure and Algorithm program ");
+    }
+}
